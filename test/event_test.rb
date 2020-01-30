04f7587f910fa40e6ee9d600868883b92b10014f
Javier Aguilar <jvr.agx@gmail.com>
2020-01-30 13:56:39 -0700

    Pass test_max_age_is_correct

--- test/event_test.rb
+++ test/event_test.rb
@@ -11,4 +11,18 @@ class EventTest < Minitest::Test
 
     assert_instance_of Event, swimming
   end
+
+  def test_max_age_is_correct
+    event = "Swimming"
+    ages = [24, 30, 18, 20, 65]
+    swimming = Event.new(event, ages)
+
+    event2 = "Bowling"
+    ages2 = [24, 30, 52, 20, 18]
+    bowling = Event.new(event2, ages2)
+
+    assert_equal 65, swimming.max_age
+    assert_equal 52, bowling.max_age
+
+  end
 end
