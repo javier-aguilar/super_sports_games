78b114c7ef27afe9e91b097a79922e173f140ae4
Javier Aguilar <jvr.agx@gmail.com>
2020-01-30 13:54:06 -0700

    Add testing for Event class

---
+++ test/event_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/games'
+require './lib/event'
+
+class EventTest < Minitest::Test
+  def test_it_exists
+    event = "Swimming"
+    ages = [24, 30, 18, 20, 41]
+    swimming = Event.new(event, ages)
+
+    assert_instance_of Event, swimming
+  end
+end
